2aecf6e91aa321eaa6fc84f51473749f34600934
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 00:31:27 -0300

    remove debg log

--- internal/infra/web/handlers_factory/make_team_handlers.go
+++ internal/infra/web/handlers_factory/make_team_handlers.go
@@ -2,7 +2,6 @@ package handlers_factory
 
 import (
 	"database/sql"
-	"fmt"
 	"goal-api/internal/infra/repository"
 	"goal-api/internal/infra/web"
 	"goal-api/internal/usecases"
@@ -12,8 +11,6 @@ func MakeTeamHandlers(db *sql.DB) *web.TeamHandlers {
 	teamRepository := repository.NewTeamRepositoryMySql(db)
 	createTeamUseCase := usecases.MakeCreateTeamUseCase(teamRepository)
 	teamHandlers := web.NewTeamHandlers(createTeamUseCase)
-
-	fmt.Println("Has passed hereeee")
 
 	return teamHandlers
 }
