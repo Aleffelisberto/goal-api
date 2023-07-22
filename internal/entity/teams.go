1c56e58ef0f83879815bb67747d5409aac44f68a
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 00:25:43 -0300

    feat: add team entities

---
+++ internal/entity/teams.go
@@ -0,0 +1,28 @@
+package entity
+
+type Team struct {
+	ID           int
+	Name         string
+	Abbreviation string
+	Country      string
+	CreatedAt    string
+	UpdatedAt    string
+}
+
+type TeamCreate struct {
+	Name         string
+	Abbreviation string
+	Country      string
+}
+
+type TeamRepository interface {
+	Create(team *TeamCreate) error
+}
+
+func NewTeamCreate(name string, abbreviation string, country string) *TeamCreate {
+	return &TeamCreate{
+		Name:         name,
+		Abbreviation: abbreviation,
+		Country:      country,
+	}
+}
