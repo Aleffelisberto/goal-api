7e4b9f7ef30130e7d3c79b7580bb17c61b909d7b
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 21:29:38 -0300

    adapt makeTeamHandlers factory to receive goqu database connection

--- internal/infra/web/handlers_factory/make_team_handlers.go
+++ internal/infra/web/handlers_factory/make_team_handlers.go
@@ -1,14 +1,15 @@
 package handlers_factory
 
 import (
-	"database/sql"
-	"goal-api/internal/infra/repository"
+	"goal-api/internal/infra/repository/rgoqu"
 	"goal-api/internal/infra/web"
 	"goal-api/internal/usecases"
+
+	"github.com/doug-martin/goqu"
 )
 
-func MakeTeamHandlers(db *sql.DB) *web.TeamHandlers {
-	teamRepository := repository.NewTeamRepositoryMySql(db)
+func MakeTeamHandlers(db *goqu.Database) *web.TeamHandlers {
+	teamRepository := rgoqu.NewTeamRepositoryGoqu(db)
 	createTeamUseCase := usecases.MakeCreateTeamUseCase(teamRepository)
 	teamHandlers := web.NewTeamHandlers(createTeamUseCase)
 
