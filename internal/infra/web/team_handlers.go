a54218dc9ede7d4a23edce4187d735e145999450
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 22:56:18 -0300

    add LisTeamsHandler

--- internal/infra/web/team_handlers.go
+++ internal/infra/web/team_handlers.go
@@ -9,11 +9,13 @@ import (
 
 type TeamHandlers struct {
 	CreateTeamUseCase *usecases.CreateTeamUseCase
+	ListTeamsUseCase  *usecases.ListTeamsUseCase
 }
 
-func NewTeamHandlers(createTeamUseCase *usecases.CreateTeamUseCase) *TeamHandlers {
+func NewTeamHandlers(createTeamUseCase *usecases.CreateTeamUseCase, listTeamsUseCase *usecases.ListTeamsUseCase) *TeamHandlers {
 	return &TeamHandlers{
 		CreateTeamUseCase: createTeamUseCase,
+		ListTeamsUseCase:  listTeamsUseCase,
 	}
 }
 
@@ -31,4 +33,15 @@ func (handler *TeamHandlers) CreateTeamHandler(w http.ResponseWriter, r *http.Re
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (handler *TeamHandlers) ListTeamsHandler(w http.ResponseWriter, r *http.Request) {
+	teams, err := handler.ListTeamsUseCase.Execute()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(teams)
 }
