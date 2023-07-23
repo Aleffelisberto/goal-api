329561ab5959c26bd2f680c2f9a029ee5bf35671
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 22:54:15 -0300

    add FindAll function signature

--- internal/entity/teams.go
+++ internal/entity/teams.go
@@ -17,6 +17,7 @@ type TeamCreate struct {
 
 type TeamRepository interface {
 	Create(team *TeamCreate) error
+	FindAll() ([]Team, error)
 }
 
 func NewTeamCreate(name string, abbreviation string, country string) *TeamCreate {
