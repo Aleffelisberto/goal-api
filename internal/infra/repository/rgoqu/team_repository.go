a71e9a32dd335119391dd1c9d19c7e0e637d73a8
Alefsandler <aleffelisberto@gmail.com>
2023-07-22 22:55:05 -0300

    add FindAll functino in TeamRepositoryGoqu

--- internal/infra/repository/rgoqu/team_repository.go
+++ internal/infra/repository/rgoqu/team_repository.go
@@ -33,3 +33,34 @@ func (repo *TeamRepositoryGoqu) Create(team *entity.TeamCreate) error {
 	}
 	return nil
 }
+
+func (repo *TeamRepositoryGoqu) FindAll() ([]entity.Team, error) {
+	query := goqu.Select().From("teams")
+
+	sql, _, err := query.ToSQL()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := repo.DB.Query(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var teams []entity.Team
+	for rows.Next() {
+		var team entity.Team
+		err := rows.Scan(&team.ID, &team.Name, &team.Abbreviation, &team.Country, &team.CreatedAt, &team.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		teams = append(teams, team)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return teams, nil
+}
